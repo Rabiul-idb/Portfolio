03dc4e23c8ec6bb66c792dfeabc4c2f461401b4e
rabiul-idb <rabiul.idb40@gmail.com>
2022-08-15 21:02:21 +0600

    style switch add

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ $('.aside-bar ul li').click(function(){
     $(this).addClass('active');
     $(this).siblings().removeClass('active');
 });
+
+
+
+// ============typing animation ==========
+
+var typed = new typed(".typing",{
+    strings:["web designer","frontend developer","psd / figma file conversion"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+
---
+++ js/style-switch.js
@@ -0,0 +1,68 @@
+// ======================style- switch toggle start ====================
+
+        const styleSwitchToggle = document.querySelector(".style-switch-toggle");
+          styleSwitchToggle.addEventListener("click", ()=>{
+            document.querySelector(".style-switch").classList.toggle("open");
+        });
+
+    // hide style-switch on scroll
+        window.addEventListener("scroll", ()=>{
+           if(document.querySelector(".style-switch").classList.contains("open")){
+                 document.querySelector(".style-switch").classList.remove('open');
+            }
+        });
+
+//================= theme color change start  ===========================
+ 
+ const alternateStyle = document.querySelectorAll(".alternate-style");
+
+ function SetThemeColor(selectThemeColor){
+    alternateStyle.forEach((style)=>{
+        const atbr = style.getAttribute("title");
+        if(selectThemeColor === atbr){
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled","true");
+        }
+    });
+ }
+
+//============== theme light and dark mode =============
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", ()=>{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+});
+
+window.addEventListener("load", () =>{
+    if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+});
+
+
+
+
+
+
+
+
+// jquery
+
+        // style- switch toggle start
+
+            //$('.style-switch-toggle').click(function(){
+            //    $('.style-switch').toggleClass('open');
+            //});
+
+        // hide style-switch on scroll
+
+            // $(window).scroll(function(){
+            //     $('.style-switch').removeClass('open');
+            // });
