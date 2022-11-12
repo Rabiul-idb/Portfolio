b710b06bf409fed4a06ed2307a38abd83ed13407
rabiul-idb <rabiul.idb40@gmail.com>
2022-11-12 20:46:23 +0600

    link updated

--- js/style-switch.js
+++ js/style-switch.js
@@ -37,7 +37,16 @@ dayNight.addEventListener("click", ()=>{
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark");
+   
+    if(document.body.classList.contains("dark")){
+        dayNight.setAttribute("title", "light mode");
+    }
+    else{
+        dayNight.setAttribute("title", "dark mode");
+    }
 });
+
+  
 
 window.addEventListener("load", () =>{
     if(document.body.classList.contains("dark")){
